8b8aae71376c1acf20948b66f2cd3e1d6978fe96
anaprado <ana.dprado@grupopan.com>
2025-01-17 16:29:27 -0300

    adicionando stop words

--- src/main/java/com/example/apache_lucene/services/SynonymAnalyzer.java
+++ src/main/java/com/example/apache_lucene/services/SynonymAnalyzer.java
@@ -1,7 +1,9 @@
 package com.example.apache_lucene.services;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
 import org.apache.lucene.analysis.synonym.SynonymMap;
@@ -10,12 +12,15 @@ import org.apache.lucene.util.CharsRef;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class SynonymAnalyzer extends Analyzer {
 
     private final SynonymMap synonymMap;
+    private final CharArraySet stopWords;
 
     public SynonymAnalyzer(String synonymFile) throws IOException {
+        this.stopWords = new CharArraySet(List.of("de", "do", "da"), true);
         SynonymMap.Builder builder = new SynonymMap.Builder(true);
         loadSynonyms(builder, synonymFile);
         synonymMap = builder.build();
@@ -38,6 +43,7 @@ public class SynonymAnalyzer extends Analyzer {
     protected TokenStreamComponents createComponents(String fieldName) {
         StandardTokenizer tokenizer = new StandardTokenizer();
         TokenStream tokenStream = new SynonymGraphFilter(tokenizer, synonymMap, true);
+        tokenStream = new StopFilter(tokenStream, stopWords);
         return new TokenStreamComponents(tokenizer, tokenStream);
     }
 }
